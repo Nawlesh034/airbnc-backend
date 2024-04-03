f6d8f22ef0fc0601db73fdd74b94d4cd5718ada0
Nawlesh034 <nn802301@gmail.com>
2024-04-03 23:35:26 +0530

    frontenf url changed

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/uploads',express.static(__dirname+'/uploads'));
 
 app.use(cors({   // for connecting frontend to backend
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'https://airbnc-frontend.vercel.app',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
