aef179b6bc869fbc07e14b8f68c01bdf1e32571c
Nawlesh034 <nn802301@gmail.com>
2025-06-01 05:55:16 +0530

    change

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post('/login',async(req,res)=>{
                res.cookie('token', token, {
   httpOnly: true,
   secure: true,
-  sameSite: 'none',
+  sameSite: 'lax',
 }).json(userDoc);
 
             });
